14bdf57d89912260417c92775bfaa50ad43c6570
ayrton <ayrton.parkinson1@gmail.com>
2023-06-16 06:50:56 -0400

    Add recursive max problem

---
+++ 04_quicksort/recursive_max.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+def recursive_max(arr)
+  return 0 if arr.empty?
+  return arr[0] if arr.length == 1
+
+  remaining_max = recursive_max(arr[1..])
+  arr[0] > remaining_max ? arr[0] : remaining_max
+end
+
+puts recursive_max([5, 1, 9, 3])
